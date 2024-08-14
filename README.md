d72fac766eb351275d7479a5e61ef5181593843e
Nyle Coelho <biancacoelhoo05@gmail.com>
2024-08-13 22:02:27 -0400

    chore: Update README.md and hangman.py files

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Jogo-da-Forca
 Projeto simples para iniciantes simulando o jogo da forca em Python. 
-oioioioi
---
+++ hangman.py
@@ -0,0 +1,32 @@
+print("Welcome to Hangman! Let's play!")
+print("I'm thinking in a programming language... Can you guess it?")
+
+import random
+words = ["python", "java", "kotlin", "javascript", "ruby", "swift", "php", "csharp", "c", "golang", "r", "perl", "scala", "rust", "haskell"]
+word = random.choice(words)
+
+guessed = ["-" for _ in word]
+print("".join(guessed))
+
+attempts = 8
+
+while attempts > 0:
+    letter = input("Input a letter: ")
+    if letter in word:
+        for i in range(len(word)):
+            if word[i] == letter:
+                guessed[i] = letter
+        print("\n" + "".join(guessed))
+    else:
+        print("No such letter in the word")
+        attempts -= 1
+    if "-" not in guessed:
+        print("You guessed the word!")
+        print("You survived!")
+        break
+
+if "-" in guessed:
+    print("You are hanged!")
+    print("The word was " + word)
+
+print("Thanks for playing!")
